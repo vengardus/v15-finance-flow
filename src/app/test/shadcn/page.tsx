90461acd1631c2e33bfcd61da7973452ba1b820b
Ismytv <ismytv@gmail.com>
2024-10-30 15:45:00 -0500

    chore:agrega shadcn, prueba button (/test/shadcn)

---
+++ src/app/test/shadcn/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function TestShadcn() {
+    return (
+        <div>
+            <div>TestShadcn</div>
+            <div className="flex gap-4">
+                <Button variant="default">Default</Button>
+                <Button variant="secondary">Secondary</Button>
+            </div>
+        </div>
+
+    )
+}
