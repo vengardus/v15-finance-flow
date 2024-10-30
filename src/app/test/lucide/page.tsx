9373535d7a598df2fba20c5163201934598494d6
Ismytv <ismytv@gmail.com>
2024-10-30 16:05:37 -0500

    chore:agrega luciode-react, prueba

---
+++ src/app/test/lucide/page.tsx
@@ -0,0 +1,11 @@
+import { Smile } from "lucide-react";
+
+
+export default function TestLucidePage() {
+    return (
+        <div className="app">
+            <Smile color="#3e9392" />
+            <Smile color="#5d4e3e" />
+        </div>
+    );
+}
